9bb66daad0c38443dd51a9ddb664dc15d124259a
fenil <fenilmodi4820@gmail.com>
2023-09-15 19:40:06 +0530

    added Analytics form vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,7 +32,10 @@ export default function RootLayout({ children }) {
       <head>
         
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+            {children}
+           <Analytics />
+       </body>
     </html>
   )
 }
