263b25545ebf8113a02ba2bd2ef99c42582af847
fenil <fenilmodi4820@gmail.com>
2023-09-17 18:01:23 +0530

    changed cofiite effect to firework style

--- src/app/page.js
+++ src/app/page.js
@@ -8,33 +8,75 @@ export default function Home() {
       <Script src="https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.12.0/tsparticles.confetti.bundle.min.js" strategy="beforeInteractive"></Script>
       <Script id="blast_effect">
         {
-          `const end = Date.now() + 15 * 150;
+          // `const end = Date.now() + 15 * 150;
 
-          // Google colors
-          const colors = ["#4285F4", "#0F9D58", "#F4B400", "#DB4437"];
+          // // Google colors
+          // const colors = ["#4285F4", "#0F9D58", "#F4B400", "#DB4437"];
           
-          (function frame() {
-            confetti({
-              particleCount: 2,
-              angle: 60,
-              spread: 55,
-              origin: { x: 0 },
-              colors: colors,
-            });
+          // (function frame() {
+          //   confetti({
+          //     particleCount: 2,
+          //     angle: 60,
+          //     spread: 55,
+          //     origin: { x: 0 },
+          //     colors: colors,
+          //   });
           
-            confetti({
-              particleCount: 2,
-              angle: 120,
-              spread: 55,
-              origin: { x: 1 },
-              colors: colors,
-            });
+          //   confetti({
+          //     particleCount: 2,
+          //     angle: 120,
+          //     spread: 55,
+          //     origin: { x: 1 },
+          //     colors: colors,
+          //   });
           
-            if (Date.now() < end) {
-              requestAnimationFrame(frame);
-            }
-          })();
-          `
+          //   if (Date.now() < end) {
+          //     requestAnimationFrame(frame);
+          //   }
+          // })();
+          // `
+
+                `const duration = 15 * 350,
+                animationEnd = Date.now() + duration,
+                defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+              
+              function randomInRange(min, max) {
+                return Math.random() * (max - min) + min;
+              }
+              
+              const interval = setInterval(function() {
+                const timeLeft = animationEnd - Date.now();
+              
+                if (timeLeft <= 0) {
+                  return clearInterval(interval);
+                }
+              
+                const particleCount = 50 * (timeLeft / duration);
+              
+                // since particles fall down, start a bit higher than random
+                confetti(
+                  Object.assign({}, defaults, {
+                    particleCount,
+                    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                    colors: ["#4285F4", "#0F9D58", "#F4B400", "#DB4437"], // Google Colors
+                    emojis: ["🔥", "🔥", "🔥", "🔥", "🔥", "🎖️", "🎖️", "🎖️"], // Fire Emojis and Medal Emojis
+                  })
+                );
+                confetti(
+                  Object.assign({}, defaults, {
+                    particleCount,
+                    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                    colors: ["#4285F4", "#0F9D58", "#F4B400", "#DB4437"], // Google Colors
+                    emojis: ["🔥", "🔥", "🔥", "🔥", "🔥", "🎖️", "🎖️", "🎖️"], // Fire Emojis and Medal Emojis
+                  })
+                );
+              }, 250);
+              
+              `
+
+
+
+
         }
       </Script>
       <nav className='w-full  shadow-md relative '>
