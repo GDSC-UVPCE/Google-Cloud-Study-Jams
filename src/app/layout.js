02f1d4d62c7ef4a781a52bd7254c12528550db1a
codingarpan <contact.codingarpan@gmail.com>
2023-09-23 12:29:21 +0530

    name field added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "GDSC UVPCE",
